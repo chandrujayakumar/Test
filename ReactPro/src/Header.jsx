08ef88ed600e61b17788a14abd068f45dd943b2c
chandrujayakumar <chandrujayakumar987@gmail.com>
2024-05-22 10:36:26 +0530

    Working on it

---
+++ ReactPro/src/Header.jsx
@@ -0,0 +1,11 @@
+function Header(){
+    return(
+        <header>
+            <h1>Hello world</h1>
+            <nav>
+                <ol></ol>
+            </nav>
+        </header>
+    );
+}
+export default Header
